52e3c39150b89dd9063bd5f87a0c2a762446a423
i4ibob <i4ibob@gmail.com>
2024-05-18 13:39:51 +0300

    Add project files.

---
+++ Homework_1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//HW L1  types and 
+// main part
+
+
+string name;
+string surname;
+int age;
+
+name = "Goga";
+surname = "Zuck";
+age = 69;
+
+Console.WriteLine(name);
+Console.WriteLine(surname);
+Console.WriteLine(age);
+
+Console.WriteLine($"\n\nHello, my name is {name} {surname} 0_0 \n  my age is {age} ");
+
+
+
+short answer = -25;
+
+while (true)
+{
+    Console.WriteLine("\nЧтоб выйти ответье на вопрос: \n Cколько будет 25-50?");
+    int userAnswer = Convert.ToInt32(Console.ReadLine());
+    if (userAnswer != answer)
+    {
+        Console.WriteLine("ответ не верный");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Подтвердите выход " +
+            "\n Press any key...");
+        break;
+    }
+  
+}
+Console.ReadKey();
+
+// Примечание.
+// Не пойму куда пропала точка входа в программу  метод Main  и class Program

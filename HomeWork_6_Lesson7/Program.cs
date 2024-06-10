f801740f5264e23f4ce47e7d3269c2c5673579db
i4ibob <i4ibob@gmail.com>
2024-06-10 22:55:10 +0300

    HW 6    3/5

---
+++ HomeWork_6_Lesson7/Program.cs
@@ -0,0 +1,246 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lesson
+{
+    public class Program
+    {
+
+
+        //Функції:
+
+        //Завдання 1.Написати функцію, яка приймає два параметри: основу ступеня та показник ступеня, та обчислює ступінь числа на основі отриманих даних.
+
+
+        public static int Square(int firstNum, int secondNum)
+        {
+            int result = firstNum;
+
+            for (int i = 1; i < secondNum; i++)
+            {
+                result *= firstNum;
+            }
+            return result;
+        } //готово
+
+        //Завдання 2. Написати функцію, яка отримує як параметри 2 цілих числа і повертає суму чисел з діапазону між ними.
+
+        public static int Range(int firstNum, int secondNum)
+        {
+            int result = 0;
+            if (firstNum < secondNum) {
+                for (int i = firstNum; i < secondNum; i++)
+                {
+                    if (i != firstNum && i != secondNum)
+                    {  result += i;
+                    }
+                }
+            }
+            else if (firstNum > secondNum) 
+            {
+                for (int i = secondNum ; i < firstNum; i++)
+                {
+                    if (i != firstNum && i != secondNum)
+                    {
+                        result += i;
+                    }
+                }
+
+
+            }
+
+
+            return result;
+        } // готово
+
+
+        //Завдання 3. Число називається досконалим, якщо сума всіх його дільників дорівнює йому самому. Напишіть функцію пошуку таких чисел у введеному інтервалі.
+
+        public static int[] Perfect(int firstNum, int secondNum)
+        {
+            firstNum =Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            int indexLength;
+
+
+            if (firstNum > secondNum)  // проверяем чтоб диаппазон был задан верно 
+            {
+                int transfer = firstNum;
+                firstNum = secondNum;
+                secondNum = transfer;
+            }
+
+
+          indexLength = secondNum - firstNum;
+            int[] TempArrayNum = new int[indexLength+1]; // создаем массив
+
+            // ищем идеальные числа
+            int count = 1;
+            int sum = 1;
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                for (int j = 2; j <= i % j;j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum+=j;
+                    }
+                }
+                if (sum == i) 
+                {
+                    TempArrayNum[count] = i;
+                    count++;
+                }
+            }
+            int[] pefectnum = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                pefectnum[i] = TempArrayNum[i];
+            }
+
+            foreach (int i in pefectnum) 
+            {
+                Console.WriteLine(i); 
+            }
+
+            return pefectnum;
+        }  //очень сложно для меня.. нужно больше времени из за отключений электричества не успеваю 
+
+        //Завдання 4. Написати функцію, яка приймає як параметр масив карт і порядковий номер картки, вивести на екран передану їй гральну картку.
+        public static void cart(int[,]ReqArray)
+        {
+            int firstNum = Convert.ToInt32(Console.ReadLine());  // 52 масимум как в колоде карт не считая джокера 
+            int secondNum = Convert.ToInt32(Console.ReadLine()); // порядковый номер карты включаеи в себя 13 номиналов
+            string[] ArrTypeOfCard =  { "туз","2","3","4","5","6","7","8","9","10","Валет","Дама","Король"}  ;
+
+
+        } //  из за отключений электричества не успеваю 
+
+
+        //Завдання 5. Написати функцію, яка визначає, чи є «щасливим» шестизначне число.
+        public static void Lucky()
+        {
+
+            
+           
+            int[] reqArr = new int[6];
+            int index = 0;
+            while (true)
+            {
+                Console.Write("Введите номер вашего билетика:");
+                string? req = Console.ReadLine();
+                if (req.Length == 6)
+                {
+                    int reqInt = Convert.ToInt32(req);
+
+                    for (int i = 5; i >= 0; i--)
+                    {
+                        reqArr[i] = reqInt % 10;
+                        reqInt /= 10;
+                    }
+                    Console.Write("Номер билета:");
+
+                    foreach (int reqArray in reqArr)
+                    { 
+                        Console.Write($"{reqArray}");
+                        index++;
+                        if (index == 3) { Console.Write("|"); }
+                        
+                    }
+                    int firstNum = reqArr[0] + reqArr[1] + reqArr[2];
+                    int secondNum = reqArr[3] + reqArr[4] + reqArr[5];
+
+                    Console.WriteLine($"\nПервая половина  чисел= {firstNum}\nВторая половина чисел = {secondNum}");
+                    if (firstNum == secondNum) Console.WriteLine("Счастливый билетик!");
+                    else Console.WriteLine("Неудачный билетик");
+
+
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Поддельный билетик");
+                    continue;
+                }
+            }
+              
+
+        } //готово
+
+            public static void Main()  // точка входа в программу, программа стартует с этой функции (метода)
+
+            {
+            Console.WriteLine($"#1 Возведение в степень\n" +
+                $"#2 Сумма диапазона чисел\n" +
+                $"#3 Идеальное числа в массиве \n" +
+                $"#4 Массив карт\n" +
+                $"#5 Счастливый билет\n" +
+                $"Выберите тип программы:");
+
+            int numProgram;
+            while (true)
+            {
+                 numProgram = Convert.ToInt32(Console.ReadLine());
+                if (numProgram < 1 || numProgram > 5) 
+                {
+                    Console.WriteLine("Неверный код программы.\nВыберите тип программы:");
+                    continue;
+                }
+                else { break; }
+
+            }
+            int firstNum;
+            int secondNum;
+
+            switch (numProgram){
+                case 1: // Возведение в степень
+                    //запрос данных
+                    Console.Write($"Введите первое число (возводимое): ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Введите второе  число (степень): ");
+                   secondNum = Convert.ToInt32(Console.ReadLine());
+                    //вызов функции
+                    Console.WriteLine(Square(firstNum , secondNum));
+                    //отчёт
+                    Console.Write($"Программа выполнена успешно.");
+                    break;
+
+
+                case 2: //Сумма диапазона чисел
+                    Console.Write($"Введите первое число диапазона: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Введите второе  число диапазона: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+                    //вызов функции
+                    Console.WriteLine(Range(firstNum, secondNum));
+                    break;
+                case 3: //Идеальное числа в массиве
+                    Console.Write($"Введите первое число диапазона: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+                    Console.Write($"Введите второе  число диапазона: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+                  
+                    Console.WriteLine($"Идекальные числа в диапазоне от {firstNum} до {secondNum}");
+                    Perfect(firstNum, secondNum);
+                    //вызов функции
+                    Console.WriteLine();
+                    break;
+                case 4: //Массив карт
+
+                    break;
+                case 5: //Счастливый билет
+
+                    Lucky();
+                    break;
+
+            }
+            }
+    }
+}
+
---
+++ Lesson_6_arrey/Program.cs
@@ -0,0 +1,272 @@
+﻿using System.Text;
+
+namespace Lesson
+{
+    public class Program
+    {
+        public static void Main()  // точка входа в программу, программа стартует с этой функции (метода)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // массив - это набор однотипных данных. все значения размещаются последовательно в оперативной памяти
+            // crud -> create, read, update, delete
+
+            //// v1
+            //int[] nums1 = new int[5] { 1, 2, 3, 4, 5 };
+            //// v2
+            //int[] nums2 = new int[] { 1, 2, 3, 4, 5 };
+            //// v3
+            //int[] nums3 = new[] { 1, 2, 3, 4, 5 };
+            //// v4
+            int[] nums4 = { 11, 22, 33, 44, 55 };
+            //// v5
+            //var nums5 = new int[10];
+
+            //// [] -> индексаторы
+            //// индекс -> порядковый номер значения в массиве (начинаются индексы с нуля)
+
+            //Console.WriteLine(nums4);
+            //Console.WriteLine(nums4[0]);
+            //Console.WriteLine(nums4[2]);
+            //Console.WriteLine(nums4[nums4.Length - 1]);
+
+            //nums4[0] = 1111;
+            //Console.WriteLine(nums4[0]);
+
+            //// v1
+            //for (int i = 0; i < nums4.Length; i++)
+            //{
+            //    Console.Write($"{nums4[i]} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            //int j = 0;
+
+            //// v2
+            //while (j < nums4.Length)
+            //{
+            //    Console.Write($"{nums4[j]} \t");
+            //    j++;
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////// v3
+            //foreach (var item in nums4)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////
+            //// заполнить массив размером 10 рандомными числами
+            //const int arrSize = 10;
+            //var arr = new int[arrSize];
+            //var rnd = new Random();
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"{arr[i]} \t");
+            //}
+            //Console.WriteLine("\n");
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = rnd.Next(1, 100);
+            //}
+
+            //foreach (var item in arr)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////Console.WriteLine($"{Guid.NewGuid()}");
+
+            ////// найти среднее арифметическое
+            ////var arrSum = 0;
+
+            ////foreach (var item in arr)
+            ////{
+            ////    arrSum += item;
+            ////}
+
+            ////Console.WriteLine($"Sum: {arrSum} avg: {(double)arrSum / arr.Length}\n");
+
+            ////// ввести с клавиатуры число и добавить его в конец массива
+            //Console.WriteLine("Enter number: ");
+            //var number = Convert.ToInt32(Console.ReadLine());
+
+            //var arr_2 = new int[arr.Length + 1];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr_2[i] = arr[i];
+            //}
+
+            //arr_2[arr.Length] = number;
+
+            //foreach (var item in arr_2)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////// delete last
+            //var arr_3 = new int[arr_2.Length - 1];
+
+            //for (int i = 0; i < arr_2.Length - 1; i++)
+            //{
+            //    arr_3[i] = arr_2[i];
+            //}
+
+            //foreach (var item in arr_3)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////// insert by index
+            //var arr_4 = new int[arr_3.Length + 1];
+            //Console.WriteLine("Enter number to insert: ");
+            //var numberToInsert = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter index for insert: ");
+            //var indexForInsert = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < arr_3.Length + 1; i++)
+            //{
+            //    if (indexForInsert < 0 || indexForInsert > arr_3.Length)
+            //    {
+            //        Console.WriteLine("Incorrect index!");
+            //        break;
+            //    }
+
+            //    if (i < indexForInsert)
+            //    {
+            //        arr_4[i] = arr_3[i];
+            //    }
+            //    else if (i > indexForInsert)
+            //    {
+            //        arr_4[i] = arr_3[i - 1];
+            //    }
+            //    else
+            //    {
+            //        arr_4[indexForInsert] = numberToInsert;
+            //    }
+            //}
+
+            //foreach (var item in arr_4)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            ////// remove by index
+            //var arr_5 = new int[arr_4.Length - 1];
+            //Console.WriteLine("Enter index for delete: ");
+            //var indexForDelete = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 0; i < arr_4.Length - 1; i++)
+            //{
+            //    if (indexForDelete < 0 || indexForDelete > arr_4.Length)
+            //    {
+            //        Console.WriteLine("Incorrect index!");
+            //        break;
+            //    }
+
+            //    if (i < indexForDelete)
+            //    {
+            //        arr_5[i] = arr_4[i];
+            //    }
+            //    else if (i >= indexForDelete)
+            //    {
+            //        arr_5[i] = arr_4[i + 1];
+            //    }
+            //}
+
+            //foreach (var item in arr_5)
+            //{
+            //    Console.Write($"{item} \t");
+            //}
+
+            //Console.WriteLine("\n");
+
+            //////
+            // int[] arr = { 1, 3, 5, 2, 3, 2, 6, 8, 2 };
+
+            // foreach (var item in arr)
+            // {
+            // Console.Write($"{item} \t");
+            // }
+
+            // Console.WriteLine("\n");
+
+            // var numToDelete = 3;
+
+            // var numToDeleteCount = 0;
+
+            // foreach (var item in arr)
+            // {
+            // if (item == numToDelete)
+            // {
+            // numToDeleteCount++;
+            // }
+            // }
+
+            // var newArr = new int[arr.Length - numToDeleteCount];
+            // var arrWithIndexesToIgnore = new int[numToDeleteCount];
+
+            // int currentIndexForArrWithIndexesToIgnore = 0;
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            // if (arr[i] == numToDelete)
+            // {
+            // arrWithIndexesToIgnore[currentIndexForArrWithIndexesToIgnore] = i;
+            // currentIndexForArrWithIndexesToIgnore++;
+            // }
+            // }
+
+            // foreach (var item in arrWithIndexesToIgnore)
+            // {
+            // Console.Write($"{item} \t");
+            // }
+
+            // Console.WriteLine("\n");
+
+            // var tempNumber = 0;
+
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            // bool isCurrentIndexFound = false;
+
+            // foreach (var item in arrWithIndexesToIgnore)
+            // {
+            // if (i == item)
+            // {
+            // isCurrentIndexFound = true;
+            // tempNumber++;
+            // break;
+            // }
+            // }
+
+            // if (!isCurrentIndexFound)
+            // {
+            // newArr[i - tempNumber] = arr[i];
+            // }
+            // }
+
+            // foreach (var item in newArr)
+            // {
+            // Console.Write($"{item} \t");
+            // }
+
+            // Console.WriteLine("\n");
+        }
+    }
+}
